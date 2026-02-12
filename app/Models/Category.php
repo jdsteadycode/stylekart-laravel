ef805a35a45d74a5e15c8fd2c372c25453d18880
jdsteadycode <desaijeet11@gmail.com>
2026-02-12 21:33:52 +0530

    Added Category Model and defined Relationships

---
+++ app/Models/Category.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+use App\Models\SubCategory;
+
+class Category extends Model
+{
+    // use SoftDeletes;
+
+    protected $fillable = ['name'];
+
+    public function subcategories()
+    {
+        return $this->hasMany(Subcategory::class);
+    }
+}
