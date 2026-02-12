35b420598aaed86eb322b2e8448871e1b70e2646
jdsteadycode <desaijeet11@gmail.com>
2026-02-12 21:34:19 +0530

    Added Subcategory Model and defined Relationships

---
+++ app/Models/Subcategory.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Category;
+use Illuminate\Support\Facades\Log;
+
+class SubCategory extends Model
+{
+    protected $fillable = ['name', 'category_id'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
