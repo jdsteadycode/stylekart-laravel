1224971f83e630da46a87d38900dbba41edb082e
jdsteadycode <desaijeet11@gmail.com>
2026-02-19 10:31:09 +0530

    Updated profile view

--- resources/views/vendor/profile/edit.blade.php
+++ resources/views/vendor/profile/edit.blade.php
@@ -7,6 +7,12 @@
     </div>
 @endif
 
+@if (session('warning'))
+    <div class="m-4 text-orange-700 bg-orange-100 px-4 py-2 rounded-md text-m">
+        {{ session('warning') }}
+    </div>
+@endif
+
 
 <div class="max-w-xl">
     <h1 class="text-2xl font-semibold mb-6">Edit Profile</h1>
