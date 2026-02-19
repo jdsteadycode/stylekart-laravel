a73a698d32648fade5cf2cce0fe06ba7b9d37e8e
jdsteadycode <desaijeet11@gmail.com>
2026-02-19 10:35:33 +0530

    Created ProductColor model and defined relationships

---
+++ app/Models/ProductColor.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Product;
+
+// grab spatie package utilties..
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+
+class ProductColor extends Model implements HasMedia
+{
+    // allow to interact with media
+    use InteractsWithMedia;
+
+    // columns to filled
+    protected $fillable = ["product_id", "name"];
+
+    // () -> related product
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    // () -> create collection
+    public function registerMediaCollections(): void
+    {
+        $this->addMediaCollection("color_images")->useDisk("public");
+    }
+
+    /*
+    Mutatots
+    */
+    // () -> standardize the color values..
+    public function setColorAttribute($colorValue)
+    {
+        // trim and standardize the color values
+        $this->attributes["color"] = trim(strtolower($colorValue));
+    }
+}
