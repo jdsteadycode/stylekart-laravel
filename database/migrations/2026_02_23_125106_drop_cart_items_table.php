e059cd826130cc0dc20a65f848f0c6f9eebe2584
jdsteadycode <desaijeet11@gmail.com>
2026-02-24 09:53:03 +0530

    Added migration for dropping the cart_items table

---
+++ database/migrations/2026_02_23_125106_drop_cart_items_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        // when migrate..
+        Schema::drop('cart_items');
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        // when rollback
+        Schema::create("cart_items", function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("user_id")->constrained()->onDelete("cascade");
+            $table->foreignId("product_id")->constrained()->onDelete("cascade");
+            $table
+                ->foreignId("variant_id")
+                ->constrained("product_variants")
+                ->onDelete("cascade");
+            $table->unsignedInteger("item_qty")->default(1);
+            $table->timestamps();
+
+            $table->unique(["user_id", "variant_id"]); // one variant per user cart
+        });
+    }
+};
